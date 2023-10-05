4a297d3c6b042b961b4b4ab52d6e91126e6ec0f1
barma <barmatey@gmail.com>
2023-10-05 11:13:51 +0300

    processing

--- src/bus/broker.py
+++ src/bus/broker.py
@@ -11,10 +11,18 @@ class Broker:
             self._subscribers[pub] = set()
         self._subscribers[pub].add(sub)
 
+    def subscribe_to_many(self, pubs, sub):
+        for pub in pubs:
+            self.subscribe(pub, sub)
+
     def unsubscribe(self, pub, sub):
         self._subscribers[pub].remove(sub)
         if len(self._subscribers[pub]) == 0:
             del self._subscribers[pub]
+
+    def unsubscribe_from_many(self, pubs, sub):
+        for pub in pubs:
+            self.unsubscribe(pub, sub)
 
     def get_subscribers(self, pub):
         return self._subscribers[pub]
--- src/spreadsheet/cell/handlers.py
+++ src/spreadsheet/cell/handlers.py
@@ -1,7 +1,8 @@
 from src.bus.eventbus import EventBus
+from src.bus.broker import Broker
+
 from .domain import CellCreated, CellUpdated, CellSubscribed, CellUnsubscribed, CellDeleted, CellSubscriber
 from .repository import CellRepo, CellRepoFake
-from ...bus.broker import Broker
 
 bus = EventBus()
 
@@ -15,4 +16,24 @@ def handle_cell_created(event: CellCreated, repo: CellRepo = CellRepoFake()):
 def handle_cell_updated(event: CellUpdated, repo: CellRepo = CellRepoFake()):
     repo.update_one(event.new_entity)
 
-    subs = Broker().get_subscribers(event.new_entity)
+    subs: list[CellSubscriber] = Broker().get_subscribers(event.new_entity)
+    for sub in subs:
+        sub.on_cell_updated(old=event.old_entity, actual=event.new_entity)
+
+
+@bus.register(CellDeleted)
+def handle_cell_deleted(event: CellDeleted, repo: CellRepo = CellRepoFake()):
+    repo.delete_one(event.entity)
+    subs: list[CellSubscriber] = Broker().get_subscribers(event.entity)
+    for sub in subs:
+        sub.on_cell_deleted(event.entity)
+
+
+@bus.register(CellSubscribed)
+def handle_cell_subscribed(event: CellSubscribed):
+    Broker().subscribe_to_many(event.pubs, event.sub)
+
+
+@bus.register(CellUnsubscribed)
+def handle_cell_unsubscribed(event: CellUnsubscribed):
+    Broker().unsubscribe_from_many(event.pubs, event.sub)
