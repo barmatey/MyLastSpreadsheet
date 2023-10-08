6099f92ec83a1319374456970cd8c65742acd0bf
barma <barmatey@gmail.com>
2023-10-08 15:14:34 +0300

    sheet_meta

--- tests/spreadsheet/test_sheet.py
+++ tests/spreadsheet/test_sheet.py
@@ -4,20 +4,15 @@ import pytest
 import pytest_asyncio
 
 import db
-from src.core import OrderBy
-from src.spreadsheet.cell.repository import CellRepoPostgres
+from src.spreadsheet.cell.entity import CellValue
 from src.spreadsheet.sheet.repository import SheetRepoPostgres
-from src.spreadsheet.sheet_info.repository import SheetInfoRepoPostgres
-from src.spreadsheet.sindex.repository import SindexRepoPostgres
 from src.spreadsheet.sheet import (entity as sheet_entity, commands as sheet_commands)
 
 
-@pytest.fixture(scope='function')
-@pytest.mark.asyncio
-async def sheet() -> sheet_entity.Sheet:
+async def create_sheet(table: list[list[CellValue]]) -> sheet_entity.Sheet:
     async with db.get_async_session() as session:
         sheet_repo = SheetRepoPostgres(session)
-        cmd = sheet_commands.CreateSheet(table=[[0, 1], [2, 3], [4, 5]], sheet_repo=sheet_repo, )
+        cmd = sheet_commands.CreateSheet(table=table, sheet_repo=sheet_repo, )
         sheet = await cmd.execute()
         await session.commit()
         return sheet
@@ -36,10 +31,11 @@ async def test_create_sheet():
 
 
 @pytest.mark.asyncio
-async def test_get_sheet_by_uuid(sheet):
-    sheet = await sheet
+async def test_get_sheet_by_uuid():
+    sheet = await create_sheet(table=[[0, 1], [2, 3], [4, 5]])
     async with db.get_async_session() as session:
         sheet_repo = SheetRepoPostgres(session)
         cmd = sheet_commands.GetSheetByUuid(uuid=sheet.sheet_info.uuid, sheet_repo=sheet_repo)
         sheet_from_repo = await cmd.execute()
         assert sheet_from_repo == sheet
+
